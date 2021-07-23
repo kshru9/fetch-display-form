d12aeea79c08a1604a9b935c12591da8e8617e34
Shruti Katpara <45987734+ShrutiKatpara@users.noreply.github.com>
2021-07-23 22:49:47 +0530

    /api/save working

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SOURCES, DATABASES } from '../all-values';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
@@ -10,34 +11,8 @@ export class FormComponent {
   title = "Your Form";
   sources = SOURCES;
   databases= DATABASES;
-  // values = {
-  //   "sources" : [
-  //     {
-  //       "default" : [
-  //         {
-  //           "medicare_demographic" : [
-  //             "name",
-  //             "id",
-  //             "county"
-  //           ]
-  //         }
-  //       ]
-  //     }, 
 
-  //     {
-  //       "source2" : [
-  //         {
-  //           "table2" : [
-  //             "col1",
-  //             "col2",
-  //             "col3"
-  //           ]
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // }
-
+  constructor ( private http: HttpClient){}
 
   // selectedValue? : Value;
   // onSelect(value: Value): void {
@@ -47,27 +22,30 @@ export class FormComponent {
   selectedSource? : string;
   onSelectSource(value: string): void {
     this.selectedSource = value;
+    console.log(this.selectedSource);
   }
 
   selectedDatabase? : string;
   onSelectDatabase(value: string): void {
     this.selectedDatabase = value;
+    console.log(this.selectedDatabase);
   }
 
-  selectedColumns? : string;
+  selectedColumns : string[] = [];
   onSelectColumns(value: string): void {
-    this.selectedColumns = value;
+    this.selectedColumns.push(value);
+    console.log(this.selectedColumns);
   }
 
-  selectedFilters? : string[];
-  onSelectFilters(value? : string[]): void {
+  selectedFilters? : string;
+  onSelectFilters(value? : string): void {
     // this.selectedFilters = value.split(",");
     this.selectedFilters = value;
     console.log(this.selectedFilters);
   }
 
   selectedLimit? : string;
-  onSelectLimit(value: string): void {
+  onSelectLimit(value? : string): void {
     this.selectedLimit = value;
     console.log(this.selectedLimit);
   }
@@ -81,8 +59,49 @@ export class FormComponent {
       "filters": this.selectedFilters,
       "limit": this.selectedLimit
     };
-
+    
     console.log(this.ans);
+  }
+  
+  filters? : string[];
+  submitForm(): void {
+    // var formData: any = new FormData();
+    // formData.append("source", this.selectedSource);
+    // formData.append("db", this.selectedDatabase);
+    // formData.append("columns", this.selectedColumns);
+    // formData.append("filters", this.selectedFilters);
+    // formData.append("limit", this.selectedLimit);
+    
+    // console.log(formData.value);
+    
+    console.log(this.selectedSource);
+    console.log(this.selectedDatabase);
+    console.log(this.selectedColumns);
+    console.log(this.selectedFilters);
+    console.log(this.selectedLimit);
+    
+    this.filters = this.selectedFilters?.split(",");
+
+    console.log(this.filters);
+
+    let json = {
+      "source" : this.selectedSource,
+      "db": this.selectedDatabase,
+      "columns": this.selectedColumns,
+      "filters": this.filters,
+      "limit": this.selectedLimit
+    };
+
+    console.log(json);
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Access-Control-Allow-Origin', 'http://localhost:4200')
+                      .set('Access-Control-Allow-Methods', "GET, POST, DELETE, PUT");
+    
+    this.http.post('http://localhost:8081/api/save', json, {headers: headers}).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
   }
   
 }
